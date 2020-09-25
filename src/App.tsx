95dca084e097c5d5f7c5c7acce7d24c559603e87
Bartłomiej Staroń <bstaron@Gispartner.pl>
2020-09-25 14:22:00 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './components/Todos'
 
 function App() {
   return (
     <div className="App">
+      <h1>TEst</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         Nazwa
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Todos />
+
       </header>
     </div>
   );
---
+++ src/components/Todos.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Todos() {
+  return (
+    <div className="Todos">
+      <h1>TODOS</h1>
+
+    </div>
+  );
+}
+
+export default Todos;
