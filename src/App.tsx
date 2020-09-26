92685fbc22557a088650e2bfd7e496ffac0fa29e
tlomiej <bartekstaron@gmail.com>
2020-09-26 16:30:28 +0200

    2 commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import React from 'react';
 import Todos from './components/Todos'
 
 function App() {
+//string test = '';
+
+
+  interface Task {
+    name: string;
+    id: number;
+  }
+
+
+
+  let state: Task = {
+    name: "test",
+    id: 12
+  } 
+
+
   return (
     <div className="App">
       <h1>TEst</h1>
       <header className="App-header">
        
+
         <p>
          TESTzwa 
         </p>
