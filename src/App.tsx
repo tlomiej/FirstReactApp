93dbf3b8c4a535fd5eb1471c19603459ed662490
tlomiej <bartekstaron@gmail.com>
2020-09-26 21:56:16 +0200

    Pierwsszy kompoennt

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Todos from './components/Todos'
+import ReactDOM from "react-dom";
 
-function App() {
-//string test = '';
+type Task = {
+  name: string;
+  id: number;
+};
 
-
-  interface Task {
-    name: string;
-    id: number;
-  }
-
+interface Tasks extends Array<Task>{};
 
 
-  let state: Task = {
+class App extends React.Component {
+  state: Tasks = [{
     name: "test4",
     id: 12
-  } 
+  },
+  {
+    name: "test5",
+    id: 15
+  },
+  {
+    name: "test6",
+    id: 16
+  },
+  {
+    name: "test7",
+    id: 17
+  } ];
+
+  textParam: String = "TestParam" 
 
 
-  return (
-    <div className="App">
+  render() {
+    return (
+      <div className="App">
       <h1>TEst</h1>
       <header className="App-header">
-       
+        <Todos  login={"test"}/>
 
-        <p>
-         TESTzwa 
-        </p>
-        <Todos />
 
+      
       </header>
     </div>
-  );
+    );
+  }
 }
+
+
+
+
 
 export default App;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Todos() {
-  return (
-    <div className="Todos">
-      <h1>TODOS</h1>
 
-    </div>
-  );
+
+export interface login {
+  login: string;
 }
+class Todos<todos> extends React.Component<login> {
+
+  
+  render() {
+    return (
+     // console.log(this.login);
+    <h1>TODOS </h1>
+
+     
+    );
+  }
+}
+
 
 export default Todos;
