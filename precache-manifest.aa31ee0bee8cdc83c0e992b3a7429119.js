e99c1b83f083c32c0f7a952471dd85a9f17290ea
Bartłomiej Staroń <bartekstaron@gmail.com>
2020-10-29 21:43:03 +0100

    Updates

---
+++ precache-manifest.aa31ee0bee8cdc83c0e992b3a7429119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61cd846e1807e0f7f8ff38bb7a5d5ae",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7dec87169afe9e77959d",
+    "url": "/my-app/static/css/main.e0414ff9.chunk.css"
+  },
+  {
+    "revision": "9a00aff9da2fdece649e",
+    "url": "/my-app/static/js/2.37ba9c2c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/static/js/2.37ba9c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dec87169afe9e77959d",
+    "url": "/my-app/static/js/main.0eb179f7.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
