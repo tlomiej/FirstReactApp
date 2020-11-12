8aaa8f6f02783aa654184e795cc1781d2cdf6795
Bartłomiej Staroń <bartekstaron@gmail.com>
2020-11-12 23:21:50 +0100

    DEV Firebase auth

--- src/components/FireBaseStart.tsx
+++ src/components/FireBaseStart.tsx
@@ -1,46 +1,67 @@
 import { IconButton } from '@material-ui/core';
 import firebase from 'firebase//app';
-//import 'firebase/auth';
+import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/firestore';
 import React from 'react';
+import useEffect from 'react';
 import { firebaseConfig } from "./../models/FirebaseConfig";
 import SearchIcon from '@material-ui/icons/Search';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+// import Dialog from '@material-ui/core/Dialog';
 
 interface Props {
     firebaseData?: any;
-    data? :any;
+    data?: any;
 }
 
 export default class FireBaseStart<Firebase> extends React.Component<Props>{
 
 
-    saveDate(){
+    saveDate() {
         firebase.firestore().collection('zgloszenia').add({
-            opis: "OPIS test...123"
-          })
+            opis: "OPIS test...123",
+            pole: "testowe",
+            numer: 13
+        })
     }
 
     async getData() {
- 
-        const dem : any= [];
+
+        const dem: any = [];
         await firebase.firestore().collection('zgloszenia').get()
-          .then(querySnapshot => {
-            querySnapshot.docs.forEach(doc => {
-            dem.push(doc.data());
-          });
-        });
+            .then(querySnapshot => {
+                querySnapshot.docs.forEach(doc => {
+                    dem.push(doc.data());
+                });
+            });
         return dem;
-      }
+    }
+
+    author(auth: any) {
+        const email = `test${Math.random()}`.replace('.', '') + `@test.pl`
+        const pass = 'haslo1234'
+        auth.createUserWithEmailAndPassword(email, pass)
+
+
+    }
+
 
     async componentDidMount() {
         // Initialize Firebase
 
         console.log("Firebase START")
         firebase.initializeApp(firebaseConfig);
+        const auth = firebase.auth()
         this.saveDate();
         let data = await this.getData();
-         console.log("Firebase KONIEC", data)
+        console.log("Firebase KONIEC", data)
+
+        this.author(auth)
+        firebase.auth().onAuthStateChanged((user) => {
+            console.log("auth???", user)
+        })
+
 
     }
 
@@ -57,6 +78,8 @@ export default class FireBaseStart<Firebase> extends React.Component<Props>{
                 <IconButton type="submit" onClick={this.onClick} className='iconButton' aria-label="search">
                     <SearchIcon />
                 </IconButton>
+
+                
             </div>
         )
     }
