affb1ac36c0b03d5acb54380b4dbf67378b144bd
Bartłomiej Staroń <bartekstaron@gmail.com>
2020-12-15 21:41:49 +0100

    DEV snackbar przy zapisie

--- src/components/edit/FormContact.tsx
+++ src/components/edit/FormContact.tsx
@@ -1,4 +1,4 @@
-import { Button, createStyles, Grid, makeStyles, TextField, Theme, IconButton } from "@material-ui/core";
+import { Button, createStyles, Grid, makeStyles, TextField, Theme } from "@material-ui/core";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Controller, useForm } from "react-hook-form";
@@ -10,6 +10,9 @@ import 'firebase/auth';
 import EditIcon from '@material-ui/icons/Edit';
 import TimelineIcon from '@material-ui/icons/Timeline';
 import EditToolbar from "./EditToolbar";
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 enum GenderEnum {
   female = "female",
@@ -49,6 +52,7 @@ interface Props {
 export default function App(props: Props) {
   const { register, handleSubmit, control } = useForm<IFormInput>();
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
 
 
   const onSubmit = (data: IFormInput) => {
@@ -59,6 +63,8 @@ export default function App(props: Props) {
       if (user) {
         fdb.firestore().collection('zgloszenia').add({ allData }).then((docRef) => {
           console.log("Document written with ID: ", docRef.id);
+          setOpen(true);
+
         })
           .catch((error) => {
             console.error("Error adding document: ", error);
@@ -70,8 +76,40 @@ export default function App(props: Props) {
 
   };
 
+  const handleClose = (event: React.SyntheticEvent | React.MouseEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
   return (
     <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Data saved"
+        action={
+          <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+              UNDO
+            </Button>
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
       <EditToolbar draw={props.draw}></EditToolbar>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Grid className={classes.paperGrid} container spacing={3} direction="column">
