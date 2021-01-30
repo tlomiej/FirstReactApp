a83652f8a9f5eb1a721194a72a2897ffce625db0
Bartłomiej Staroń <bartekstaron@gmail.com>
2021-01-30 22:31:32 +0100

    DEV - redux...

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from "redux";
+import store from './redux/store'
 
 
 ReactDOM.render(
